bc8d14ca2f50b9d1c2fca0579f89d3fa61fa8c7d
zohrehshahabi <zshahabiprogrammer@gmail.com>
2022-12-24 10:29:12 +0330

    add  sample fpr YML config

---
+++ src/main/java/com/exercise/demo/SpringBootAppDemoApplication.java
@@ -0,0 +1,17 @@
+package com.exercise.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Properties;
+
+@SpringBootApplication
+public class SpringBootAppDemoApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SpringBootAppDemoApplication.class,args);
+
+	}
+
+}
